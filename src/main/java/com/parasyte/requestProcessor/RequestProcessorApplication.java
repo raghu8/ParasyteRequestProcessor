e0ef373198c1a1c6cef1f6edf90f5b2b3278b7cc
raghu8 <rvmanthena@gmail.com>
2022-03-12 10:48:45 -0500

    project files

---
+++ src/main/java/com/parasyte/requestProcessor/RequestProcessorApplication.java
@@ -0,0 +1,13 @@
+package com.parasyte.requestProcessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RequestProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RequestProcessorApplication.class, args);
+	}
+
+}
